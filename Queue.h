3fa67240928306b11dc4e53496e759eef62ae0eb
suye8013 <44388064+suye8013@users.noreply.github.com>
2018-11-30 10:00:58 +0800

    Create Queue.h

---
+++ Queue.h
@@ -0,0 +1,27 @@
+#ifndef __QUEUE_H__
+#define __QUEUE_H__
+
+typedef int DataType;
+
+typedef struct QueueNode
+{
+	DataType _data;
+	struct QueueNode* _next;
+}QueueNode;
+
+typedef struct Queue
+{
+	QueueNode* _head;
+	QueueNode* _tail;
+}Queue;
+
+void QueueInit(Queue* q);
+void QueuePush(Queue* q, DataType x);
+void QueuePop(Queue* q);
+DataType QueueFront(Queue* q);
+DataType QueueBack(Queue* q);
+size_t QueueSize(Queue* q);
+int QueueEmpty(Queue* q);
+
+
+#endif
